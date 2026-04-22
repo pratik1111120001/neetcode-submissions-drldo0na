ef49cf01c591d92e450f66817bf760d9cc2184fa
pratik1111120001 <65250631+pratik1111120001@users.noreply.github.com>
2026-04-22 23:16:08 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int sum = (n * (n+1))/2;
+        int curr_sum = 0;
+        for(auto it : nums) curr_sum += it;
+        return (sum - curr_sum);
+    }
+};
